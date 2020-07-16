ca95f125d666f0a84c7cc4e7d1ee1d453807687b
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-16 17:54:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop(string)
+end
+
+def pop_with_args(array,times)
+  array.pop * times
+
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array, times)
+  array.shift * times
+  
+end
